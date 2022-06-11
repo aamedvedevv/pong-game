96060d3314009e139e5818e1266d59646ed2e6d4
Bagg Piter <baggpite@oa-f4.msk.21-school.ru>
2022-06-11 21:29:52 +0300

    Develop. Created main.c file.

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,22 @@
 //I WANT TO PLAY WITH YOU
 //        YOUR FRIEND, AI
 
+#include <stdio.h>
+#include <stdlib.h>
 
 
+int main() {
+ int rocket_1_Y = 19;       // Переменные по умолчанию - положение рок 1
+ int rocket_2_Y = 5;        // Переменные по умолчанию - положение рок 2
+ int ball_X = 41;           // Переменные по умолчанию - положение мячик
+ int ball_Y = 13;
+ int vector_X = -1;         // Направление движения мячика
+ int vector_Y = 1;
+ int scorePlayer_1 = 0;     // счет 1 игрока
+ int scorePlayer_2 = 0;     // счет 2 игрока
+    
+    while (1){
+        
+    }
+    return 0;
+}
